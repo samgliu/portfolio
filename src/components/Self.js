0dfcfaf2417c76d5cc8a6c3990844873be20e920
samgliu <icefire587@gmail.com>
2022-01-23 00:09:12 -0500

    English name

--- src/components/Self.js
+++ src/components/Self.js
@@ -2,7 +2,7 @@ function Self() {
     return (
         <div className="self">
             <p>
-                My name is Gang Liu. I graduated from school with a Computer
+                My name is Sam Liu. I graduated from school with a Computer
                 Science A.S. degree in Dec. 2021. Meanwhile, I finished The Odin
                 Project with excellent front-end, back-end, and full-stack
                 projects. I am very motivated to learn and code.
