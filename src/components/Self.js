ebf656daba8616d71b76210e9040465fa2385ccb
samgliu <icefire587@gmail.com>
2022-01-23 00:20:09 -0500

    self

--- src/components/Self.js
+++ src/components/Self.js
@@ -5,7 +5,7 @@ function Self() {
                 My name is Sam Liu. I graduated from school with a Computer
                 Science A.S. degree in Dec. 2021. Meanwhile, I finished The Odin
                 Project with excellent front-end, back-end, and full-stack
-                projects. I am very motivated to learn and code.
+                projects. I am motivated to learn and code.
             </p>
         </div>
     );
