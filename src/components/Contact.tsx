3876a6c8c1ad3d1587f9ce2fdd3bae20053b91e2
Sam Liu <samgliu19@gmail.com>
2023-04-24 20:20:27 -0400

    style: add style to card

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,29 +1,30 @@
+import { FaEnvelope, FaGithub, FaLinkedin } from 'react-icons/fa';
+
 import React from 'react';
-import { FaLinkedin, FaGithub, FaEnvelope } from 'react-icons/fa';
 
 function Contact() {
-    return (
-        <div className="contact" id="contact">
-            <div>
-                <a href="https://github.com/samgliu">
-                    <FaGithub />
-                    Github
-                </a>
-            </div>
-            <div>
-                <a href="https://linkedin.com/in/gang-liu-78b432124">
-                    <FaLinkedin />
-                    Linkedin
-                </a>
-            </div>
-            <div>
-                <a href="mailto:samgliu19@gmail.com">
-                    <FaEnvelope />
-                    Email<span className="switch">: samgliu19@gmail.com</span>
-                </a>
-            </div>
-        </div>
-    );
+  return (
+    <div className="contact" id="contact">
+      <div>
+        <a href="https://github.com/samgliu">
+          <FaGithub />
+          Github
+        </a>
+      </div>
+      <div>
+        <a href="https://www.linkedin.com/in/sam-liu-78b432124/">
+          <FaLinkedin />
+          Linkedin
+        </a>
+      </div>
+      <div>
+        <a href="mailto:samgliu19@gmail.com">
+          <FaEnvelope />
+          Email<span className="switch">: samgliu19@gmail.com</span>
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,55 +1,96 @@
-import React from "react";
-import { ProjectType } from "./Portfolios";
+import { ProjectType } from './Portfolios';
+import React from 'react';
 const { v4: uuidv4 } = require('uuid');
 
 interface Props {
-    project: ProjectType;
+  project: ProjectType;
 }
 
 function Portfolio({ project }: Props) {
-    if (project) {
-        return (
-            <div className="portfolio" id="portfolio">
-                <h2>{project.name}</h2>
-                <div className="builtwith">
-                    {project.builtwith.map((el: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined) => {
-                        return <span key={uuidv4()}>{el}</span>;
-                    })}
-                </div>
+  if (project) {
+    return (
+      <div
+        className="portfolio"
+        id="portfolio"
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '1rem',
+            textAlign: 'center',
+          }}
+        >
+          <h2>{project.name}</h2>
+          <div className="builtwith">
+            {project.builtwith.map(
+              (
+                el:
+                  | boolean
+                  | React.ReactChild
+                  | React.ReactFragment
+                  | React.ReactPortal
+                  | null
+                  | undefined
+              ) => {
+                return <span key={uuidv4()}>{el}</span>;
+              }
+            )}
+          </div>
+        </div>
 
-                <a
-                    href={project.link}
-                    target="_blank"
-                    rel="noreferrer noopener"
-                >
-                    <img src={project.picture} alt="" />
-                </a>
-                <p>{project.description}</p>
-                <div className="features-container">
-                    {project.features.map((feature) => {
-                        return <span>{feature}</span>;
-                    })}
-                </div>
-
-                <a
-                    href={project.link}
-                    target="_blank"
-                    rel="noreferrer noopener"
-                >
-                    Live Demo
-                </a>
-                <a
-                    href={project.repo}
-                    target="_blank"
-                    rel="noreferrer noopener"
-                >
-                    Repository
-                </a>
-            </div>
-        );
-    } else {
-        return null;
-    }
+        <a href={project.link} target="_blank" rel="noreferrer noopener">
+          <img src={project.picture} alt="" />
+        </a>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '1rem',
+          }}
+        >
+          <p>{project.description}</p>
+          <div
+            className="features-container"
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            {project.features.map((feature) => {
+              return <span>{feature}</span>;
+            })}
+          </div>
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
+              gap: '0.5rem',
+            }}
+          >
+            <a href={project.link} target="_blank" rel="noreferrer noopener">
+              Live Demo
+            </a>
+            <a href={project.repo} target="_blank" rel="noreferrer noopener">
+              Repository
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
 
 export default Portfolio;
